311cf2f9a7aeeafab3b04ec9c9451f9e96b5aff5
Akshat Bamania <akshat.bamania23@gmail.com>
2026-01-30 23:28:01 +0530

    small changes

--- Astrato/src/App.jsx
+++ Astrato/src/App.jsx
@@ -1,12 +1,14 @@
 
 import "./app.scss"
 import Nav from "./components/Nav/Nav"
+import Page1 from "./components/page1/Page1"
 
 function App() {
 
   return (
     <>
       <Nav />
+      <Page1 />
     </>
   )
 }
---
+++ Astrato/src/components/page1/Page1.jsx
@@ -0,0 +1,11 @@
+import "./page1.scss";
+
+function Page1(){
+    
+    return <div className="page1">
+        <p>Ignition the Spark of Inspiration</p>
+        <h1>Unleash Your Brand <span className="centerLogo"> <img src="/centerLogo.png" alt="" /> </span>with Our <span className="magico">Ḿagico</span></h1>
+    </div>
+}
+
+export default Page1
